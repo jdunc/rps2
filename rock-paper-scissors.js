de68ec41f9aa5f64ec90f442582001b8236196f3
Jordan Duncan <j.duncan@rochester.edu>
2016-10-19 15:53:03 -0500

    rpsls with buttons and counters

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,110 +1,129 @@
-$(document).ready(function (){
-  userChoice = prompt("Do you choose rock, paper, scissors, spock or lizard?");
-  if(userChoice == "lizard" || userChoice == "rock" || userChoice == "spock" || userChoice == "paper" || userChoice == "scissors"){
-  } else{
-    prompt("please choose one of the five options");
-    $(".again").css("display","inline");
-    $(".compare").css("display","none");
 
-  }
-});
-
-
-var computerChoice = Math.random();
-
+computerChoice = Math.random();
 if (computerChoice <= 0.20) {    
-  computerChoice = "rock";
+  computerChoice = "rock";
 } else if (computerChoice <= 0.40) {
-  computerChoice = "paper";
+  computerChoice = "paper";
 } else if (computerChoice <= 0.60) {
-  computerChoice = "scissors";
+  computerChoice = "scissors";
 }else if (computerChoice <= 0.80) {
-  computerChoice = "spock";
+  computerChoice = "spock";
 }else {
-  computerChoice = "lizard";
+  computerChoice = "lizard";
 }
+
+function newChoice(){
+  computerChoice = Math.random();
+  if (computerChoice <= 0.20) {    
+    computerChoice = "rock";
+  } else if (computerChoice <= 0.40) {
+    computerChoice = "paper";
+  } else if (computerChoice <= 0.60) {
+    computerChoice = "scissors";
+  }else if (computerChoice <= 0.80) {
+    computerChoice = "spock";
+  }else {
+    computerChoice = "lizard";
+  }
+}
+yourScore = 0;
+compScore = 0;
 var playAgain = function(){
   document.location.reload();
 
 };
 var compare = function(userChoice, computerChoice) {
+  newChoice()
   console.log(userChoice);
-if (userChoice === computerChoice) {
-    $("#result").html("The result is a tie!");
+  if (userChoice === computerChoice) {
+    $("#result").html("The result is a tie!");
     $(".again").css("display","inline");
-
-  } else if (userChoice === "rock") {
-    if (computerChoice === "scissors" ) {
-      $("#result").html("You Win! Rock beats Scissors.");
-$(".again").css("display","inline");
-
-    } else if (computerChoice === "lizard" ) {
-      $("#result").html("You Win! Rock beats Lizard.");
-$(".again").css("display","inline");
-
-    } else{
-      $("#result").html("You Lose! Paper and Spock beats Rock.");
-$(".again").css("display","inline");
-
-    }
-  } else if (userChoice === "paper") {
-    if (computerChoice === "rock") {
-      $("#result").html("You Win! Paper beats Rock.");
-$(".again").css("display","inline");
-
-    } else if (computerChoice === "spock") {
-      $("#result").html("You Win! Paper beats Spock.");
-$(".again").css("display","inline");
-
-    } else {
-      $("#result").html("You Lose! Scissors and Lizard beats Paper.");
-$(".again").css("display","inline");
-
-    }
-  } else if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
-      $("#result").html("You Win! Scissors beats Rock.");
-$(".again").css("display","inline");
-
-    } else if (computerChoice === "lizard") {
-      $("#result").html("You Win! Scissors beats Lizard.");
-$(".again").css("display","inline");
-
-    } else{
-      $("#result").html("You Lose! Rock and Spock beats Scissors.");
-$(".again").css("display","inline");
-
-    }
-  } else if (userChoice === "lizard") {
-    if (computerChoice === "paper") {
-      $("#result").html("You Win! Lizard beats Paper.");
-$(".again").css("display","inline");
-
-    } else if (computerChoice === "spock") {
-      $("#result").html("You Win! Lizard beats Spock.");
-$(".again").css("display","inline");
-
-    } else{
-      $("#result").html("You Lose! Scissors and Rock beats Lizard.");
-$(".again").css("display","inline");
-
-    }
-  } else if (userChoice === "spock") {
-    if (computerChoice === "scissors") {
-      $("#result").html("You Win! Spock beats Scissors.");
-$(".again").css("display","inline");
-
-    } else if (computerChoice === "rock") {
-      $("#result").html("You Win! Spock beats Rock.");
-$(".again").css("display","inline");
-
-    } else{
-      $("#result").html("You Lose! Lizard and Paper beats Spock.");
-$(".again").css("display","inline");
-
-    }
-  }
+    $("#computerChoice").html('The computer chose ' + computerChoice);
+  } else if (userChoice === "rock") {
+    if (computerChoice === "scissors" ) {
+      $("#result").html("You Win! Rock beats Scissors.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else if (computerChoice === "lizard" ) {
+      $("#result").html("You Win! Rock beats Lizard.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else{
+      $("#result").html("You Lose! Paper and Spock beats Rock.");
+      compScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    }
+  } else if (userChoice === "paper") {
+    if (computerChoice === "rock") {
+      $("#result").html("You Win! Paper beats Rock.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else if (computerChoice === "spock") {
+      $("#result").html("You Win! Paper beats Spock.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else {
+      $("#result").html("You Lose! Scissors and Lizard beats Paper.");
+      compScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    }
+  } else if (userChoice === "scissors") {
+    if (computerChoice === "paper") {
+      $("#result").html("You Win! Scissors beats Paper.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else if (computerChoice === "lizard") {
+      $("#result").html("You Win! Scissors beats Lizard.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else{
+      $("#result").html("You Lose! Rock and Spock beats Scissors.");
+      compScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    }
+  } else if (userChoice === "lizard") {
+    if (computerChoice === "paper") {
+      $("#result").html("You Win! Lizard beats Paper.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else if (computerChoice === "spock") {
+      $("#result").html("You Win! Lizard beats Spock.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else{
+      $("#result").html("You Lose! Scissors and Rock beats Lizard.");
+      compScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    }
+  } else if (userChoice === "spock") {
+    if (computerChoice === "scissors") {
+      $("#result").html("You Win! Spock beats Scissors.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else if (computerChoice === "rock") {
+      $("#result").html("You Win! Spock beats Rock.");
+      yourScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    } else{
+      $("#result").html("You Lose! Lizard and Paper beats Spock.");
+      compScore++;
+      $(".again").css("display","inline");
+      $("#computerChoice").html('The computer chose ' + computerChoice);
+    }``
+  }
+  $("#myscore").html('Your score is ' + yourScore + ' and the computers score is ' + compScore);
 };
-
-
-       
